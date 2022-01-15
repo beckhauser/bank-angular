9c14b7c6e4f1647f2957781c282880eb89baad36
Beckhauser <beckhauser125@gmail.com>
2022-01-14 21:20:06 -0300

    Removendo passagem por emit

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,27 +1,36 @@
+import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
 
 @Component({
   selector: 'nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  valor: number;
+  destino: number;
 
-  @Output() aoTransferir = new EventEmitter<any>()
+  constructor(private service: TransferenciaService) {}
 
-  valor: number
-  destino: number
-
-  transferir(){
-    console.log('Solicitada nova transferencia')
-    const novoEvento = {valor: this.valor, destino:this.destino}
-    this.aoTransferir.emit(novoEvento)
-    this.limpaCampos()
+  transferir() {
+    console.log('Solicitada nova transferencia');
+    const novoEvento: Transferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+    this.service.adicionar(novoEvento).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limpaCampos();
+      },
+      (error) => console.log(error)
+    );
   }
 
-  limpaCampos(){
-   this.valor = null
-   this.destino = null
+  limpaCampos() {
+    this.valor = null;
+    this.destino = null;
   }
 }
